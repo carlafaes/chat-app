5e7cfa74983a26dc95d8d27015d9bdc61c0b2d15
carlafaes <carlafaes18@hotmail.com>
2022-07-24 22:56:33 -0300

    mongodb connect

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express=require('express');
+const cors=require('cors');
+const mongoose=require('mongoose');
+
+
+const app=express();
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+const adminPassword = encodeURIComponent( process.env.MONGODB_PASS )
+//CONNECT TO MONGO DB
+mongoose.connect(process.env.MONGO_URL,{useNewUrlParser:true,useUnifiedTopology:true})
+.then(()=>console.log('DB Connected'))
+.catch(err=>console.log(err));
+
+const server= app.listen(process.env.PORT, ()=>{
+    console.log(`Server is running on port ${process.env.PORT}`);
+})
+
