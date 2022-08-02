3abe2855d988929d7ec1b63f5e2298bb2de81e27
carlafaes <carlafaes18@hotmail.com>
2022-08-01 22:19:27 -0300

    routes post api/auth/register

--- server/index.js
+++ server/index.js
@@ -2,12 +2,17 @@ const express=require('express');
 const cors=require('cors');
 const mongoose=require('mongoose');
 
+//routes
+const userRoutes=require('./routes/userRoutes');
+
 
 const app=express();
 require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/auth',userRoutes);
 
 const adminPassword = encodeURIComponent( process.env.MONGODB_PASS )
 //CONNECT TO MONGO DB
