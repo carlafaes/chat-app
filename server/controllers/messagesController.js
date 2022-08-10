1c7e539855aecfc3597558036bc828fa7518f5c5
carlafaes <carlafaes18@hotmail.com>
2022-08-09 23:12:46 -0300

    socket io

--- server/controllers/messagesController.js
+++ server/controllers/messagesController.js
@@ -1,55 +1,55 @@
-const Messages= require('../model/messageModel');
+const Messages = require('../model/messageModel');
 
 module.exports.addMessage = async (req, res, next) => {
-    console.log('controller addMessage', req.body);
-    try {
-        const { 
-            from,
-            to,
-            message } = req.body;
-        const data= await Messages.create({
-            message:{
-                text:message,
-            },
-            users:[from,to],
-            sender:from,
-        });
-        if(data){
-            return res.status(201).json({ status: true, msg: 'Mensaje enviado' });
-        }
-        else{
-            return res,json({ status: false, msg: 'Error al enviar mensaje' });
-        }
-
-    } catch (err) {
-        return res.status(500).json({
-            message: 'error registrando mensaje',
-            error: err,
-        });
+  console.log('controller addMessage', req.body);
+  try {
+    const {
+      from,
+      to,
+      message } = req.body;
+    const data = await Messages.create({
+      message: {
+        text: message,
+      },
+      users: [from, to],
+      sender: from,
+    });
+    if (data) {
+      return res.status(201).json({ status: true, msg: 'Mensaje enviado' });
     }
+    else {
+      return res, json({ status: false, msg: 'Error al enviar mensaje' });
+    }
+
+  } catch (err) {
+    return res.status(500).json({
+      message: 'error registrando mensaje',
+      error: err,
+    });
+  }
 }
 
 module.exports.getAllMessages = async (req, res, next) => {
-    try {
-        const { from, to } = req.body;//from y to son los usuarios que estan en la sesion
-        console.log('controller getAllMessages', req.body);
-    
-        const messages = await Messages.find({
-          users: {
-            $all: [from, to],
-          },
-        }).sort({ updatedAt: 1 });//sort by date
-         console.log('messages', messages);
-    
-        const projectedMessages = messages.map((msg) => {
-          return {
-            fromSelf: msg.sender.toString() === from,//si el mensaje es del usuario que esta en la sesion
-            message: msg.message.text,//message text
-          };
-        });
-        res.json(projectedMessages);
-      } catch (ex) {
-        console.log(err,'error');
-        next(ex);
-      }
+  try {
+    const { from, to } = req.body;//from y to son los usuarios que estan en la sesion
+    console.log('controller getAllMessages', req.body);
+
+    const messages = await Messages.find({
+      users: {
+        $all: [from, to],
+      },
+    }).sort({ updatedAt: 1 });//sort by date
+    console.log('messages', messages);
+
+    const projectedMessages = messages.map((msg) => {
+      return {
+        fromSelf: msg.sender.toString() === from,//si el mensaje es del usuario que esta en la sesion
+        message: msg.message.text,//message text
+      };
+    });
+    res.json(projectedMessages);
+  } catch (ex) {
+    console.log(err, 'error');
+    next(ex);
+  }
 }
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const cors=require('cors');
 const mongoose=require('mongoose');
-
+const socket=require('socket.io');
 //routes
 const userRoutes=require('./routes/userRoutes');
 const messagesRoutes=require('./routes/messagesRoutes');
@@ -26,3 +26,27 @@ const server= app.listen(process.env.PORT, ()=>{
     console.log(`Server is running on port ${process.env.PORT}`);
 })
 
+//SOCKET IO
+const io=socket(server, { 
+    cors:{ //cors para que se pueda conectar desde cualquier lugar
+        origins:'*:*',//origins:'http://localhost:3000'
+        credentials:true,
+    },
+});
+
+global.onlineUsers= new Map();//mapa de usuarios conectados
+
+io.on('connection',(socket)=>{//cuando se conecta un usuario
+    global.chatSocket=socket;//socket de chat
+    socket.on('add-user', (userId)=>{
+        onlineUsers.set(userId, socket.id);//agrega el usuario al mapa
+    });
+
+    socket.on('send-msg', (data)=>{//cuando se envia un mensaje
+        const sendUserSocket = onlineUsers.get(data.to);//socket del usuario a quien se envia el mensaje
+
+        if(sendUserSocket){
+            socket.to(sendUserSocket).emit('mag-receive', data.msg);//emite el mensaje al usuario a quien se envia el mensaje
+        }
+    })
+});
