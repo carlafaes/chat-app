478d75ae534e85c3dfdf3eba8cb0f8830197483d
carlafaes <carlafaes18@hotmail.com>
2022-08-11 18:50:24 -0300

    xx

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -43,13 +43,7 @@ module.exports.login = async (req, res, next) => {
     try {
 
         const { email, password } = req.body;
-        // const usernameCheck= await User.findOne({username});
-        // if(usernameCheck){
-        //     return res.json({
-        //         message:'El nombre de usuario ya existe',
-        //         status:false
-        //     });
-        // }
+     
         const emailCheck = await User.findOne({ email });
         if (!emailCheck) {
             return res.json({
